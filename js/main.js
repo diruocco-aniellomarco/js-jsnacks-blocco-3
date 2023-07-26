f844d06be7f1ac3df7e9ca0a6eb593782ed8c3ac
diruocco-aniellomarco <aniello.marco.dr@gmail.com>
2023-07-26 16:12:23 +0200

    esercizio 2

--- js/main.js
+++ js/main.js
@@ -1,32 +1,40 @@
-const firstNumber = parseInt(prompt(' Inserisci il primo numero: '));
-const secondNumber = parseInt(prompt(' Inserisci il secondo  numero: '));
+// const firstNumber = parseInt(prompt(' Inserisci il primo numero: '));
+// const secondNumber = parseInt(prompt(' Inserisci il secondo  numero: '));
 
 
+// if (firstNumber > secondNumber) {
 
-
-
-if (firstNumber > secondNumber) {
-
-    let random = Math.floor(Math.random() * (firstNumber - secondNumber) ) + secondNumber;
+//     let random = Math.floor(Math.random() * (firstNumber - secondNumber) ) + secondNumber;
     
-    console.log('primo numero ' + firstNumber);
-    console.log('secondo numero ' + secondNumber);
-    console.log('il primo è più grande del secondo ' + random);
-} 
+//     console.log('primo numero ' + firstNumber);
+//     console.log('secondo numero ' + secondNumber);
+//     console.log('il primo è più grande del secondo ' + random);
+// } 
 
-if (firstNumber < secondNumber) {
-    let random = Math.floor(Math.random() * (secondNumber -firstNumber) ) + firstNumber;
+// if (firstNumber < secondNumber) {
+//     let random = Math.floor(Math.random() * (secondNumber -firstNumber) ) + firstNumber;
 
     
-    console.log('primo numero ' + firstNumber);
-    console.log('secondo numero ' + secondNumber);
-    console.log('il secondo è più grande del primo ' + random);
+//     console.log('primo numero ' + firstNumber);
+//     console.log('secondo numero ' + secondNumber);
+//     console.log('il secondo è più grande del primo ' + random);
+// }
+
+// if (firstNumber == secondNumber) {
+    
+//     console.log('primo numero ' + firstNumber);
+//     console.log('secondo numero ' + secondNumber);
+//     console.log('I due numeri sono uguali ' + random);
+// }
+
+const firstW = prompt('Scrivi la prima parola');
+const secondW = prompt('Scrivi la seconda parola');
+
+if (firstW.length == secondW.length) {
+    console.log(firstW);
+    console.log(secondW);
+} else if (firstW.length > secondW.length) {
+    console.log(firstW);
+} else {
+    console.log(secondW);
 }
-
-if (firstNumber == secondNumber) {
-    
-    console.log('primo numero ' + firstNumber);
-    console.log('secondo numero ' + secondNumber);
-    console.log('I due numeri sono uguali ' + random);
-}
-
--- readme.md
+++ readme.md
@@ -1,3 +1,10 @@
 # esercizio 1
 
 Chiedi all'utente un primo numero, poi un secondo e genera un numero casuale compreso tra i due.
+
+# esercizio 2
+
+Dare la possibilità di inserire due parole. 
+Verificare che le due parole abbiano la stessa lunghezza. Se hanno la stessa lunghezza, stamparle entrambe altrimenti stampare la più lunga delle due.
+
+
