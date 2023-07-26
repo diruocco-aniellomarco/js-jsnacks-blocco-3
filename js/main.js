0ceee8305ab016d30858a26f86b93001055f73f2
diruocco-aniellomarco <aniello.marco.dr@gmail.com>
2023-07-26 17:23:21 +0200

    esercizio 4 + esercizio 5

--- js/main.js
+++ js/main.js
@@ -39,20 +39,44 @@
 //     console.log(secondW);
 // }
 
+// Esercizio 3 e 4
+// let listNum =[];
 
-let listNum =[];
+// let sum = 0;
 
-let sum = 0;
-while (sum < 50) {
-    let number = parseInt(prompt(' Inserisci il numero: '));
-    sum = sum + number;
-    if (sum < 50) {
-        listNum.push(number);
+// let cont = 0;
+
+// let media;
+// while (sum < 50) {
+//     let number = parseInt(prompt(' Inserisci il numero: '));
+//     sum = sum + number;
+//     cont++;
+   
+//     if (sum > 50) {
+//         media= sum / cont;
+//     }
+//     console.log('Il numero inserito è: ' + number);
+    
+    
+// }
+// console.log(listNum);
+// console.log('La somma è:  ' + sum);
+// console.log('La media è:  ' + media);
+
+
+
+//ESERCIZIO 5
+    let numbers = [1, 2, 3, 4, 5, 6, 7, 8];
+    console.log(numbers.length);
+    let evenNumbers = []; //array non variabile
+    //numbers.length - 2 (perché devo arrivare fino al valore 6) e non meno 1 ; i++ senza ";"
+    for (let i = 0; i < numbers.length - 1; i++) {
+        //numbers[i] e non numbers poi != e non =
+        if (numbers[i] % 2 == 0) {
+            //devo inserire i valori di numbers
+            evenNumbers.push(i);
+        }
     }
-    console.log('Il numero inserito è: ' + number);
-    
-    
-}
-console.log(listNum);
-console.log('La somma è:  ' + sum);
+    console.log(evenNumbers);
 
+    // dovrebbe restituire [1,3,5]
--- readme.md
+++ readme.md
@@ -11,3 +11,23 @@ Verificare che le due parole abbiano la stessa lunghezza. Se hanno la stessa lun
 # esercizio 3
 
 Crea un array vuoto e chiedi all'utente un numero da inserire nell'array. Continua a chiedere i numeri all'utente e a inserirli nell'array fino a quando la somma degli elementi è minore di 50.
+
+# esercizio 4
+Crea un array vuoto e chiedi all'utente un numero da inserire nell'array. Continua a chiedere i numeri all'utente e a inserirli nell'array fino a quando la somma degli elementi è minore di 50.
+Calcola la media dei numeri inseriti.
+
+# esercizio 5
+
+Trovare gli errori nel seguente codice:
+
+--------------------------
+    let numbers = [1, 2, 3, 4, 5, 6, 7, 8];
+    let evenNumbers = "";
+    for (let i = 0; i < numbers.length - 1; i++;) {
+        if (numbers % 2 = 0); {
+            evenNumbers.push(i);
+        }
+    }
+    console.log(evenNumbers);
+
+    // dovrebbe restituire [1,3,5]	
